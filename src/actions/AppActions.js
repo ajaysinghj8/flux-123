ac3db168b510a89fe0f402faddfaf6659481f84c
Ajay Singh <meajaysingh@hotmail.com>
2015-10-24 23:31:08 +0530

    ActionHandler

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,10 +1,10 @@
 import {dispatcher} from '../dispatcher/dispatcher';
 import {CONSTANTS}  from '../constants/constants';
 
-export let handler={};
+export let ActionHandler={};
 for (let key in CONSTANTS) {
   let funcName = key.split('_').map((word, i) => { if (i === 0) { return word.toLowerCase(); } return word[0] + word.slice(1).toLowerCase(); }).join('');
-  handler[funcName]= ((data)=>{
+  ActionHandler[funcName]= ((data)=>{
     dispatcher.dispatch({
       actionType:CONSTANTS[key],
       data:data
