5ff7699f4699e9e3c7cd7fae7c832d0dbf4ae04d
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 04:28:44 +0530

    refactory

--- src/AppBootstrap.js
+++ src/AppBootstrap.js
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import * as ReactRouter from 'react-router';
-import {ChirpStore} from './stores/ChirpStore';
-import {Api} from './utils/api';
+import {ChirpApi} from './utils/chirpApi';
 import {MainAppCmp} from './components/App';
 import {Home} from './components/home'
-let api = new Api();
-api.fetchChirps();
-let chirps = new ChirpStore();
+import {UserApi} from './utils/userApi';
+import {UserList} from './components/userList';
+
+let chirpApi = new ChirpApi();
+let userApi =new UserApi();
+userApi.fetch();
+chirpApi.fetch();
 
 let Route = ReactRouter.Route;
 let routes = (<Route handler={ MainAppCmp } >
 	<Route name='home' path= '/' handler= { Home } />
+	<Route name='users' handler= { UserList } />
+	<Route name='user' path= '/user/:id' handler= { Home } />
+
 	</Route>);
 ReactRouter.run(routes, ReactRouter.HistoryLocation, (Root) => {
 	React.render(<Root />,document.body);
