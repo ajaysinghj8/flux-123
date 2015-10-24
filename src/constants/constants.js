2a4d5fd1eea8bc542f36deb5110a91471bb2ef39
Ajay Singh <meajaysingh@hotmail.com>
2015-10-24 18:39:11 +0530

    App constants

---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+export let CONSTANTS = {
+    CHIRP: Symbol('CHIRP'),
+    CHIRPED: Symbol('CHIRPED'),
+    GOT_CHIRPS: Symbol('GOT_CHIRPS'),
+
+
+    GOT_CURRENT_USER: Symbol('GOT_CURRENT_USER'),
+    GOT_USERS: Symbol('GOT_USERS'),
+
+    FOLLOW: Symbol('FOLLOW'),
+    FOLLOWED: Symbol('FOLLOWED'),
+
+    UNFOLLOW: Symbol('UNFOLLOW'),
+    UNFOLLOWED: Symbol('UNFOLLOWED')
+
+}
