0b8ec72b6b191e001cf93f4c8f4d79fdb2a1e452
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 04:30:40 +0530

    updating home cmp

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,34 @@
 import * as React from 'react';
 import {ChirpInput} from './chirpinput';
 import {ActionHandler} from '../actions/AppActions';
-
+import {ChirpStore} from '../stores/ChirpStore';
+import {ChirpList} from './chirpList';
+let chirpStore = new ChirpStore();
 export let Home = React.createClass({
-	render: function() {
+	getInitialState: function () {
+		return {
+			chirps: chirpStore.all()
+		};
+	},
+	componentDidMount: function () {
+		chirpStore.addChangeListener(this.onChange);
+	},
+	onChange: function () {
+		this.setState({
+			chirps: chirpStore.all()
+		});
+	},
+	componentWillUnmount: function () {
+		chirpStore.removeChangeListener(this.onChange);
+	},
+	saveChirp: function (text) {
+		ActionHandler.chirp(text);
+	},
+	render: function () {
 		return (<div>
 			<ChirpInput onSave={ this.saveChirp } />
-			</div>);
-	},
-	saveChirp: function(text) {
-		ActionHandler.chirp(text);
+			<ChirpList chirps={ this.state.chirps } />
+			< /div>);
 	}
 });
 
