c97bf649660a9983f9fa99088738da9711edd0b1
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 04:28:11 +0530

    removing user api from chirpapi

--- src/utils/chirpApi.js
+++ src/utils/chirpApi.js
@@ -1,14 +1,49 @@
 import {ActionHandler} from '../actions/AppActions';
+import {dispatcher} from '../dispatcher/dispatcher';
+import {CONSTANTS} from '../constants/constants';
 
-export class Api{
+export class ChirpApi{
 	constructor(){
+		this.incrementalId =1;
 		this._chirps =[{
 			cid:1,
-			title:'Good Life',
+			text:'Good Life',
 			date: new Date()
 		}];
 	}
-	fetchChirps(){
+	fetch(){
 			 Promise.resolve(this._chirps).then(ActionHandler.gotChirps.bind(ActionHandler));
 	}
+	save(chirp){
+		var newChirp ={
+			cid: ++this.incrementalId,
+			text: chirp,
+			date: new Date()
+		}
+		this._chirps.push(newChirp);
+		Promise.resolve(newChirp).then(ActionHandler.chirped.bind(ActionHandler));
+	}
 }
+
+
+export class UsersApi{
+	constructor(){
+		
+	}
+	fetch(){
+		Promise.resolve({userId:1,fullname:'Ajay Singh', username:'plutazoo'}).then(ActionHandler.gotUsers.bind(ActionHandler));	
+	}
+	
+}
+
+
+let chirpApiInstance = new ChirpApi();
+let usersApiInstance = new UsersApi();
+
+dispatcher.register(action=>{
+	switch(action.actionType){
+		case CONSTANTS.CHIRP :
+			chirpApiInstance.save(action.data);
+			break;			
+	}
+});
