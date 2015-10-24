4829a2e54b47e05fc20941b294780b94f8463aa8
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 04:27:05 +0530

    api for user

---
+++ src/utils/userApi.js
@@ -0,0 +1,24 @@
+import {ActionHandler} from '../actions/AppActions';
+import {dispatcher} from '../dispatcher/dispatcher';
+import {CONSTANTS} from '../constants/constants';
+
+export class UserApi {
+	constructor() {
+		this._data = [
+			{ cid: 1, fullname: 'Ajay Singh', username: 'plutazoo' },
+			{ cid: 2, fullname: 'xyz abc', username: 'ss' },
+			{ cid: 3, fullname: 'info', username: 'info' }
+		];
+	}
+	fetch() {
+		Promise.resolve(this._data).then(ActionHandler.gotUsers.bind(ActionHandler));
+	}
+
+}
+
+let usersApiInstance = new UserApi();
+
+dispatcher.register(action=> {
+	switch (action.actionType) {
+	}
+});
