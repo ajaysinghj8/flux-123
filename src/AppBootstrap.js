169cca9763143469916fe8fadada31345bbcaa96
Ajay Singh <meajaysingh@hotmail.com>
2015-10-24 23:29:23 +0530

    setting up react router

--- src/AppBootstrap.js
+++ src/AppBootstrap.js
@@ -1,9 +1,17 @@
-import {React} from 'react';
-import {dispatcher} from './dispatcher/dispatcher';
-import {handler} from './actions/AppActions';
+import * as React from 'react';
+import * as ReactRouter from 'react-router';
 import {ChirpStore} from './stores/ChirpStore';
 import {Api} from './utils/api';
-var api = new Api();
-console.log(api.fetchChirps());
-var chirps = new ChirpStore();
-console.log(chirps);
+import {MainAppCmp} from './components/App';
+import {Home} from './components/home'
+let api = new Api();
+api.fetchChirps();
+let chirps = new ChirpStore();
+
+let Route = ReactRouter.Route;
+let routes = (<Route handler={ MainAppCmp } >
+	<Route name='home' path= '/' handler= { Home } />
+	</Route>);
+ReactRouter.run(routes, ReactRouter.HistoryLocation, (Root) => {
+	React.render(<Root />,document.body);
+});
