b6dd156dacc9313b82ac096513282fe11c165ffb
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 18:47:53 +0530

    fix error

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 import crypto from 'crypto';
-export let avatar = email => {
+export let avatar = (email => {
 	if (!email) return '';
 	email = crypto.createHash('md5').update(email).digest('hex');
 	return 'http://www.gravatar.com/avatar/' + email + '?s=92';
-}
+});
