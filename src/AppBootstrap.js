e4f3499896b746cdcf55d9cf96b6ff6ffeadfcca
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 18:49:36 +0530

    ref fix

--- src/AppBootstrap.js
+++ src/AppBootstrap.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactRouter from 'react-router';
 import ChirpApi from './utils/chirpApi';
 import MainAppCmp from './components/App';
-import Home from './components/home'
+import Chirp from './components/chirp'
 import UserApi from './utils/userApi';
 import UserList from './components/userList';
 
@@ -13,9 +13,9 @@ chirpApi.fetch();
 
 const {Route} = ReactRouter;
 let routes = (<Route handler={ MainAppCmp } >
-	<Route name='home' path= '/' handler= { Home } />
+	<Route name='home' path= '/' handler= { Chirp } />
 	<Route name='users' handler= { UserList } />
-	<Route name='user' path= '/user/:id' handler= { Home } />
+	<Route name='user' path= '/user/:id' handler= { Chirp } />
 
 	</Route>);
 ReactRouter.run(routes, ReactRouter.HistoryLocation, (Root) => {
