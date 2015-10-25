5270dfcf9d63e547aef95b288654048e6f0d71a5
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 15:33:27 +0530

    default export and es6

--- src/components/chirpinput.js
+++ src/components/chirpinput.js
@@ -1,42 +1,38 @@
-import * as React from 'react';
+import React from 'react';
 
 
-export let ChirpInput = React.createClass({
-	getInitialState:function(){
-		return{
-		  value:''	
-		};
-	},
-	handleChange:function(evt){
+export default class ChirpInput extends React.Components {
+	state = { value: '' }
+	handleChange = evt => {
 		this.setState({
-			value:evt.target.value
+			value: evt.target.value
 		});
-	},
-	handleClick:function(evt){
+	}
+	handleClick =evt => {
 		this.props.onSave(this.state.value);
 		this.setState({
-			value:''
+			value: ''
 		});
-	},
-	render:function(){
-		return (<div className='row'>
-			    <div className='nine columns'>
-				  <input 
-				    className='u-full-width'
-					type='text'
-					placeholder='write'
-					value={this.state.value}
-					onChange={this.handleChange} />
-				</div>
-				<div className='three columns'>
-				  <button 
-				     className='u-full-width button-primary'
-					 onClick={this.handleClick}>
-					 Chirp
-				</button>	 
-					 
-				</div>
-		  </div>);
 	}
-	
-});
+	render() {
+		return (<div className='row' >
+			<div className='nine columns' >
+			<input 
+				    className='u-full-width'
+		type = 'text'
+		placeholder = 'write'
+		value = { this.state.value }
+		onChange = { this.handleChange } />
+		</div>
+		< div className= 'three columns' >
+		<button 
+				     className='u-full-width button-primary'
+		onClick = { this.handleClick } >
+		Chirp
+		< /button>
+
+		< /div>
+		< /div>);
+	}
+
+}
