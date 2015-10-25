fed541b4451de5585d55f4b0812e020814c36096
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 15:32:31 +0530

    es6 updates

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -4,11 +4,11 @@ import {CONSTANTS}  from '../constants/constants';
 export let ActionHandler={};
 for (let key in CONSTANTS) {
   let funcName = key.split('_').map((word, i) => { if (i === 0) { return word.toLowerCase(); } return word[0] + word.slice(1).toLowerCase(); }).join('');
-  ActionHandler[funcName]= ((data)=>{
+  ActionHandler[funcName]= data =>{
     dispatcher.dispatch({
       actionType:CONSTANTS[key],
       data:data
     });
-  });
+  };
 }
 
