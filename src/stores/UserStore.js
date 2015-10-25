c0b1fb0fbcd8111fdcfad352777376af41b4fc47
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 18:53:27 +0530

    bind user store for events signedout signedin

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -4,12 +4,14 @@ import Store from './Store';
 export default class UserStore extends Store {
 	constructor() {
 		super();
-		this.currentUser = window.USER;
+		this.currentUser = {};
 	}
 	init() {
 		this.bind(CONSTANTS.GOT_USERS, this.set);
 		this.bind(CONSTANTS.FOLLOWED, this.updateUser);
 		this.bind(CONSTANTS.UNFOLLOWED, this.updateUser);
+		this.bind(CONSTANTS.SIGNEDIN_USER, this.updateUser);
+		this.bind(CONSTANTS.SIGNEDOUT_USER, this.updateUser);
 	}
 	updateUser(data) {
 		this.currentUser = data;
