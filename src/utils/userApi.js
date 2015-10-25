ff575439fcb70ad45a1ef099f6ae0516c1af5057
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 18:48:27 +0530

    using firebase for user auth

--- src/utils/userApi.js
+++ src/utils/userApi.js
@@ -1,24 +1,41 @@
 import {ActionHandler} from '../actions/AppActions';
 import {dispatcher} from '../dispatcher/dispatcher';
 import {CONSTANTS} from '../constants/constants';
+import {firebase} from './firebaseApi';
 
 export default class UserApi {
 	constructor() {
-		this._data = [
-			{ cid: 1, fullname: 'Ajay Singh', username: 'plutazoo' },
-			{ cid: 2, fullname: 'xyz abc', username: 'ss' },
-			{ cid: 3, fullname: 'info', username: 'info' }
-		];
+		firebase.onAuth(authData=> {
+			if (authData)
+				ActionHandler.signedinUser.call(ActionHandler, authData[authData.provider]);
+			else
+				setTimeout(() => {
+					ActionHandler.signedoutUser.call(ActionHandler, {});
+				}, 300);
+		});
 	}
-	fetch() {
-		Promise.resolve(this._data).then(ActionHandler.gotUsers.bind(ActionHandler));
-	}
+	signin() {
+		firebase.authWithOAuthPopup("google", ()=>{}, {
+			remember: "sessionOnly",
+			scope: "email"	
+		});
 
+	}
+	signout() {
+		firebase.unauth();
+	}
 }
+
 
 let usersApiInstance = new UserApi();
 
 dispatcher.register(action=> {
 	switch (action.actionType) {
+		case CONSTANTS.SIGNOUT_USER:
+			usersApiInstance.signout();
+			break;
+		case CONSTANTS.SIGNIN_USER:
+			usersApiInstance.signin();
+			break;
 	}
 });
