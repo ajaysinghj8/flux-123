c3d8fac83ef3e0b2277078a459a28c07bbde4600
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 18:46:40 +0530

    adding firebase api

---
+++ src/utils/firebaseApi.js
@@ -0,0 +1,6 @@
+import Firebase from 'firebase';
+export const firebase = new Firebase('https://flux-123.firebaseio.com/');
+export const chirpDb = new Firebase('https://flux-123.firebaseio.com/chirps');
+
+
+
