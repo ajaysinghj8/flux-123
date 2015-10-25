688883fecfc20745f15afef09e6ab68e9f6c9e4f
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 23:08:57 +0530

    updated firebse for users

--- src/utils/firebaseApi.js
+++ src/utils/firebaseApi.js
@@ -1,6 +1,6 @@
 import Firebase from 'firebase';
 export const firebase = new Firebase('https://flux-123.firebaseio.com/');
-export const chirpDb = new Firebase('https://flux-123.firebaseio.com/chirps');
-
+export const ChirpDb = new Firebase('https://flux-123.firebaseio.com/chirps');
+export const UsersDb = new Firebase('https://flux-123.firebaseio.com/users');
 
 
