2aa67a6f7bc57b8336c9b00d957afa54e3a56da4
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 15:34:03 +0530

    updates using es6 features

--- src/components/home.js
+++ src/components/home.js
@@ -1,34 +1,32 @@
-import * as React from 'react';
-import {ChirpInput} from './chirpinput';
+import React from 'react';
+import ChirpInput from './chirpinput';
 import {ActionHandler} from '../actions/AppActions';
-import {ChirpStore} from '../stores/ChirpStore';
-import {ChirpList} from './chirpList';
+import ChirpStore from '../stores/ChirpStore';
+import ChirpList from './chirpList';
 let chirpStore = new ChirpStore();
-export let Home = React.createClass({
-	getInitialState: function () {
-		return {
-			chirps: chirpStore.all()
-		};
-	},
-	componentDidMount: function () {
+export default class Home extends React.Components {
+	state = {
+		chirps: chirpStore.all()
+	}
+	componentDidMount() {
 		chirpStore.addChangeListener(this.onChange);
-	},
-	onChange: function () {
+	}
+	onChange() {
 		this.setState({
 			chirps: chirpStore.all()
 		});
-	},
-	componentWillUnmount: function () {
+	}
+	componentWillUnmount() {
 		chirpStore.removeChangeListener(this.onChange);
-	},
-	saveChirp: function (text) {
+	}
+	saveChirp = text => {
 		ActionHandler.chirp(text);
-	},
-	render: function () {
+	}
+	render() {
 		return (<div>
 			<ChirpInput onSave={ this.saveChirp } />
 			<ChirpList chirps={ this.state.chirps } />
 			< /div>);
 	}
-});
+}
 
