5a3c48ff7ff684ec18e0736ab95b962b1011c8b6
Ajay Singh <meajaysingh@hotmail.com>
2015-10-25 15:33:21 +0530

    default export and es6

--- src/components/chirpList.js
+++ src/components/chirpList.js
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import {ChirpBox} from './chirpBox';
+import React from 'react';
+import ChirpBox from './chirpBox';
 
-export let ChirpList = React.createClass({
-	render:function(){
+export default class ChirpList extends React.Components{	
+	  render(){
 		var listItems = this.props.chirps.map(function(chirp){
 			return <ChirpBox key={ chirp.cid } chirp={chirp} />;
 		});
 		return (<ul> {listItems} </ul>);
 	}
-});
+}
