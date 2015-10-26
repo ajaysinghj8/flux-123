9db54e7f69f3a1d547f939909714beddb56e391c
Ajay Singh <meajaysingh@hotmail.com>
2015-10-26 11:44:02 +0530

    populated chirps with users

--- src/stores/ChirpStore.js
+++ src/stores/ChirpStore.js
@@ -1,5 +1,8 @@
 import {CONSTANTS} from '../constants/constants';
 import Store from './Store';
+import UserStore from './UserStore';
+var userStore = new UserStore();
+
 export default class ChirpStore extends Store{
 	constructor(){
 		super();
@@ -7,5 +10,15 @@ export default class ChirpStore extends Store{
 	init(){
 		this.bind(CONSTANTS.GOT_CHIRPS, this.set);
 		this.bind(CONSTANTS.CHIRPED, this.add);
+	}
+	timeline(){
+		var ids = [userStore.currentUser.userId].concat(userStore.currentUser.following);
+		return this._data.filter(chirp=>ids.indexOf(chirp.userId)>-1).map(chirp=>{
+			return this.populateUserInfo(chirp);
+		});
+	}
+	populateUserInfo(chirp){
+		chirp._user = userStore.get(chirp.userId);
+		return chirp;
 	}
 }
