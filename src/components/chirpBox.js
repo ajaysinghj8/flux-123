8a1e4ec0b7e322c45b345d68da7a5611bb10c155
Ajay Singh <meajaysingh@hotmail.com>
2015-10-26 11:46:17 +0530

    rendering prepopulated chirps with user

--- src/components/chirpBox.js
+++ src/components/chirpBox.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router';
-import {avatar} from '../utils/utils';
 import Moment from 'moment';
-import UserStore from '../stores/UserStore';
-
-let userStore = new UserStore();
 
 export default class ChirpBox extends React.Component{
   render() {
     var c = this.props.chirp;
-    var user = userStore.get(c.userId);
-    if(!user) return (<p>Loading ...</p>);
     return (<li className= 'row chirp'>
-       <Link className='two columns' to='user' params={{id:user.cid}}>
-         <img src={ user.picture } />
+       <Link className='two columns' to='user' params={{id:c.userId}}>
+         <img src={ c._user.picture } />
        </Link>
        <div className='ten columns'>
             <p>
-               <strong>{c.name}</strong>
+               <strong>{c._user.name}</strong>
                <span className='timestamp'>
                 @{Moment(c.created).fromNow()}
                </span>
